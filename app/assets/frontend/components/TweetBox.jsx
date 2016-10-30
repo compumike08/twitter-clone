78eb81010f4072b8997115ae016d09075b8e6940
compumike08 <compumike08@users.noreply.github.com>
2016-10-29 21:34:32 -0400

    Add Materialize CSS styling classes & structure to TweetBox component

--- app/assets/frontend/components/TweetBox.jsx
+++ app/assets/frontend/components/TweetBox.jsx
@@ -1,11 +1,15 @@
 export default class TweetBox extends React.Component {
   render() {
     return (
-      <div>
-        <form>
-          <textarea />
-          <label>What's happening?</label>
-          <button>Tweet</button>
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s12">
+              <textarea id="new-tweet-text-input" className="materialize-textarea" />
+              <label htmlFor="new-tweet-text-input">What's happening?</label>
+              <button className="btn right">Tweet</button>
+            </div>
+          </div>
         </form>
       </div>
     )
