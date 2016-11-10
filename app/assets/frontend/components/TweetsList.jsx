cdbed03dfe139fbaf8d074459225dac576761ee0
compumike08 <compumike08@users.noreply.github.com>
2016-11-09 19:21:07 -0500

    Render li elements in TweetsList with Materialize stylings

--- app/assets/frontend/components/TweetsList.jsx
+++ app/assets/frontend/components/TweetsList.jsx
@@ -2,10 +2,22 @@ export default class TweetsList extends React.Component {
   render () {
     return (
       <div>
-        <ul>
-          <li>Tweet One...</li>
-          <li>Tweet Two...</li>
-          <li>Tweet Three...</li>
+        <ul className="collection">
+          <li className="collection-item avatar">
+            <i className="material-icons circle">person_pin</i>
+            <span className="title">User 1</span>
+            <p>My #FirstTweet</p>
+          </li>
+          <li className="collection-item avatar">
+            <i className="material-icons circle">person_pin</i>
+            <span className="title">User 1</span>
+            <p>My #SecondTweet</p>
+          </li>
+          <li className="collection-item avatar">
+            <i className="material-icons circle">person_pin</i>
+            <span className="title">User 1</span>
+            <p>My #ThirdTweet</p>
+          </li>
         </ul>
       </div>
     );
