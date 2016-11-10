6ca34b93c5912374ad85cb662a78434ac821dea8
compumike08 <compumike08@users.noreply.github.com>
2016-11-09 20:04:12 -0500

    Remove hardcoded initial sample tweet data

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,17 +1,11 @@
 import TweetBox from "./components/TweetBox";
 import TweetsList from "./components/TweetsList";
 
-let mockTweets = [
-  {id: 1, name: 'User 1', body: 'My #FirstTweet'},
-  {id: 2, name: 'User 1', body: 'My #SecondTweet'},
-  {id: 3, name: 'User 1', body: 'My #ThirdTweet'}
-];
-
 class Main extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { tweetsList: mockTweets };
+    this.state = { tweetsList: [] };
 
     //Bind 'this' context to component functions
     this.addTweet = this.addTweet.bind(this);
