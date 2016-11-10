2e08d61208f57a63c7a1999bf4a5909394058bc1
compumike08 <compumike08@users.noreply.github.com>
2016-11-09 19:35:41 -0500

    Render tweets dynamically from mock data source

--- app/assets/frontend/components/TweetsList.jsx
+++ app/assets/frontend/components/TweetsList.jsx
@@ -2,12 +2,13 @@ import Tweet from "./Tweet";
 
 export default class TweetsList extends React.Component {
   render () {
+    let tweets = this.props.tweets.map(tweet =>
+      <Tweet key={tweet.id} {...tweet} />
+    );
     return (
       <div>
         <ul className="collection">
-          <Tweet />
-          <Tweet />
-          <Tweet />
+          {tweets}
         </ul>
       </div>
     );
--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,12 +1,18 @@
 import TweetBox from "./components/TweetBox";
 import TweetsList from "./components/TweetsList";
 
+let mockTweets = [
+  {id: 1, name: 'User 1', body: 'My #FirstTweet'},
+  {id: 2, name: 'User 1', body: 'My #SecondTweet'},
+  {id: 3, name: 'User 1', body: 'My #ThirdTweet'}
+];
+
 class Main extends React.Component {
     render() {
         return (
           <div className="container">
             <TweetBox />
-            <TweetsList />
+            <TweetsList tweets={mockTweets} />
           </div>
         );
     }
