be1774ae465a0c14d2b5ab0f170944d68414a38a
michael-fei <michael-fei@users.noreply.github.com>
2016-12-21 23:22:54 -0500

    Implement update method in profile_controller

--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,6 +5,12 @@ class ProfileController < ApplicationController
   end
 
   def update
-    redirect_to profile_path
+    current_user.update(user_params)
+    redirect_to root_path
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name)
   end
 end
