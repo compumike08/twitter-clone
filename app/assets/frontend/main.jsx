2b1fe6d2aa5a7338db3ad800817d3fba952aebbb
michael-fei <michael-fei@users.noreply.github.com>
2016-12-28 22:44:58 -0500

    Fix code formatting in main.jsx

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -2,48 +2,48 @@ import TweetBox from "./components/TweetBox";
 import TweetsList from "./components/TweetsList";
 
 class Main extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = { tweetsList: [] };
+        this.state = {tweetsList: []};
 
-    //Bind 'this' context to component functions
-    this.addTweet = this.addTweet.bind(this);
-  }
+        //Bind 'this' context to component functions
+        this.addTweet = this.addTweet.bind(this);
+    }
 
-  componentDidMount(){
-      $.ajax("/tweets")
-          .success(data => this.setState({tweetsList: data}))
-          .error(error => console.log(error));
-  }
+    componentDidMount() {
+        $.ajax("/tweets")
+            .success(data => this.setState({tweetsList: data}))
+            .error(error => console.log(error));
+    }
 
-  addTweet(tweetToAdd) {
-    let newTweetsList = this.state.tweetsList;
+    addTweet(tweetToAdd) {
+        let newTweetsList = this.state.tweetsList;
 
-    newTweetsList.unshift({
-      id: Date.now(),
-      name: 'Guest',
-      body: tweetToAdd
-    });
+        newTweetsList.unshift({
+            id: Date.now(),
+            name: 'Guest',
+            body: tweetToAdd
+        });
 
-    this.setState({
-      tweetsList: newTweetsList
-    });
-  }
+        this.setState({
+            tweetsList: newTweetsList
+        });
+    }
 
-  render() {
-      return (
-        <div className="container">
-          <TweetBox sendTweet={this.addTweet} />
-          <TweetsList tweets={this.state.tweetsList} />
-        </div>
-      );
-  }
+    render() {
+        return (
+            <div className="container">
+                <TweetBox sendTweet={this.addTweet}/>
+                <TweetsList tweets={this.state.tweetsList}/>
+            </div>
+        );
+    }
 }
 
 let documentReady = () => {
     let reactNode = document.getElementById('react');
-    if(reactNode) {
+    if (reactNode) {
         ReactDOM.render(<Main />, reactNode);
     }
 };
