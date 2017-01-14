775bcb4344c145bedf331051c54b3ecc543c3163
michael-fei <michael-fei@users.noreply.github.com>
2017-01-13 20:32:25 -0500

    Refactor app to use react-router

--- app/assets/frontend/main.jsx
+++ app/assets/frontend/main.jsx
@@ -1,11 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, Link} from 'react-router';
+
 import Index from './components/Index';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        {this.props.children}
+      </div>
+    );
+  }
+}
 
 let documentReady = () => {
   let reactNode = document.getElementById('react');
   if (reactNode) {
-    ReactDOM.render(<Index />, reactNode);
+    ReactDOM.render(
+      <Router>
+        <Route component={App}>
+          <Route path="/" component={Index} />
+        </Route>
+      </Router>,
+      reactNode);
   }
 };
 
