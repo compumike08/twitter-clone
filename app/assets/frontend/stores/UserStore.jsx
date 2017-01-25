b4cef2abd0ebfc00aa974a281de48df4c152ea7c
michael-fei <michael-fei@users.noreply.github.com>
2017-01-24 19:12:02 -0500

    Remove fake user data from UserStore

--- app/assets/frontend/stores/UserStore.jsx
+++ app/assets/frontend/stores/UserStore.jsx
@@ -2,7 +2,7 @@ import AppDispatcher from "../dispatcher";
 import ActionTypes from "../constants";
 import AppEventEmitter from "./AppEventEmitter";
 
-let _users = [{id: 1, name: 'User1'}, {id: 2, name: 'User2'}];
+let _users = [];
 
 class UserEventEmitter extends AppEventEmitter {
   getAll() {
